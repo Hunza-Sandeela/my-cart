396b320fc09e1f42adfab02df5e4af3befbc4ce8
Hunza-Sandeela <hunzasandeelacs.gmail.com>
2026-03-14 01:38:33 +0500

    step 1 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Header from "./components/header.js"
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+<div>
+    <h1>TO DO LIST </h1>
+    <p>lets do it !</p>
+</div>)
+}
+
+export default Header;
